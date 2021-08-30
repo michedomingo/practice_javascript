aa1aee177e8ce7994c124e1e81a8964a52588acb
michedomingo <michedomingo@gmail.com>
2021-08-30 08:57:06 -0700

    Get current hour, minute, and second with Date() object (06_04)

--- Clock/script.js
+++ Clock/script.js
@@ -6,6 +6,14 @@ let hrPosition = 20;
 let minPosition = 130;
 let secPosition = 267;
 
+// Get current hour, minute, and second with Date() object
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log('Hour: ' + hr + ' Minute: ' + min + ' Second: ' + sec);
+
 // Apply Position numbers HANDS above as degrees in the inline styles
 HOURHAND.style.transform = 'rotate(' + hrPosition + 'deg)';
 MINUTEHAND.style.transform = 'rotate(' + minPosition + 'deg)';
